f50cba1812b9a74a84e36f31932e3defbf1f1ea1
j-knakagami2 <j-knakagami2@PC145-25.sophia.local>
2018-04-20 14:19:23 +0900

    オープンキャンパス用 BBSのWebアプリ 4月20日更新

---
+++ src/java/ejb/OcUserTbl.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import beans.OcUser;
+import beans.OcUserRequestBean;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author j-knakagami2
+ */
+@Stateless
+public class OcUserTbl {
+    @Inject
+    private OcUserRequestBean userBean;
+    @PersistenceContext
+    private EntityManager em;
+
+    public void create(OcUser wUser){
+        em.persist(wUser);
+        em.flush();
+    }
+    
+    public List<OcUser> getAll(){
+        return em.createQuery("select o from OcUser o", OcUser.class).getResultList();
+    }
+
+    //IDとパスワードでログインできるかチェックする
+    //リターン値：true　ログインできる
+    public  OcUser loginCheck(String wId,String wPass){
+        boolean flg=false;
+        OcUser wUser =null;
+        TypedQuery<OcUser> query = em.createQuery("select o from OcUser o where o.user_id = ?1 AND o.user_pass = ?2",OcUser.class);
+        query.setParameter(1, wId);
+        query.setParameter(2, wPass);
+        try{
+        wUser = query.getSingleResult();
+        }catch(NoResultException err){
+            userBean.setErrMsg("IDかパスワードが違います");
+        }
+        if(wUser !=null)
+            flg=true;
+        else
+            System.out.println("loginCheck return is null !!!");
+        return wUser;        
+    }
+    
+    public OcUser find(String wId){
+        return em.find(OcUser.class, wId);
+    }
+    
+    public void  delete(String wId){
+        OcUser wUser = em.find(OcUser.class, wId);
+        em.remove(wUser);
+    }
+}
